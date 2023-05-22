9cedab0268f673db248971310bc9fb4e68fcc9a6
ShreyashBhoir <s.bhoir@maxxton.com>
2023-05-22 14:29:43 +0530

    1st commit

---
+++ TndT/src/test/java/com/maxxton/tour/TndTApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxxton.tour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TndTApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
