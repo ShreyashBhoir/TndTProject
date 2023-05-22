52b594a407bf025d1a0724d2daf0cd9db9b61246
NayanJ12 <nayanj651@gmail.com>
2023-05-22 17:56:26 +0530

    Booking and Tour entity updated

---
+++ TndT/src/main/java/com/maxxton/tour/entities/Booking.java
@@ -0,0 +1,18 @@
+package com.maxxton.tour.entities;
+
+import java.util.Date;
+
+public class Booking {
+	
+	//data fields
+	private Date bookingdate;
+	private int groupsize;
+	private double price;
+	
+	//tour and user mapping
+	
+	private Tour tour;
+	private User user;
+	
+
+}
--- TndT/src/main/java/com/maxxton/tour/entities/Tour.java
+++ TndT/src/main/java/com/maxxton/tour/entities/Tour.java
@@ -23,6 +23,117 @@ public class Tour {
 	//review entity mapping
 	@OneToMany(mappedBy = "reviewid" ,cascade = CascadeType.REMOVE)
 	private Review review;
+
+	
+
+	//getters and setters
+	public int getTourid() {
+		return tourid;
+	}
+
+	public void setTourid(int tourid) {
+		this.tourid = tourid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public int getDuration() {
+		return duration;
+	}
+
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+
+	public int getGroupSize() {
+		return groupSize;
+	}
+
+	public void setGroupSize(int groupSize) {
+		this.groupSize = groupSize;
+	}
+
+	public String getDifficulty() {
+		return difficulty;
+	}
+
+	public void setDifficulty(String difficulty) {
+		this.difficulty = difficulty;
+	}
+
+	public int getRating() {
+		return rating;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getBegindate() {
+		return begindate;
+	}
+
+	public void setBegindate(Date begindate) {
+		this.begindate = begindate;
+	}
+
+	public String getImageurl() {
+		return imageurl;
+	}
+
+	public void setImageurl(String imageurl) {
+		this.imageurl = imageurl;
+	}
+
+	public Review getReview() {
+		return review;
+	}
+
+	public void setReview(Review review) {
+		this.review = review;
+	}
+
+	//ToString
+	@Override
+	public String toString() {
+		return "Tour [tourid=" + tourid + ", name=" + name + ", location=" + location + ", duration=" + duration
+				+ ", groupSize=" + groupSize + ", difficulty=" + difficulty + ", rating=" + rating + ", price=" + price
+				+ ", description=" + description + ", begindate=" + begindate + ", imageurl=" + imageurl + ", review="
+				+ review + "]";
+	}
+	
+	
+	
 	
 
 }
