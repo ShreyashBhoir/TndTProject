9a225ac43d20db2cd33a3c4ed18888538d0b06f3
Akash123-coder <kumarakashhzb2001@gmail.com>
2023-05-22 17:54:56 +0530

    User Table Creation and Review Getter Setter

--- TndT/src/main/java/com/maxxton/tour/entities/Review.java
+++ TndT/src/main/java/com/maxxton/tour/entities/Review.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
@@ -27,6 +28,49 @@ public class Review {
 	private Date dateofcreation;
 	
 	@ManyToOne
-	Tour tour;
+	@JoinColumn(name="tourid")
+	private Tour tour;
+
+	public int getReviewid() {
+		return reviewid;
+	}
+
+	public void setReviewid(int reviewid) {
+		this.reviewid = reviewid;
+	}
+
+	public String getReview() {
+		return review;
+	}
+
+	public void setReview(String review) {
+		this.review = review;
+	}
+
+	public int getRating() {
+		return rating;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+
+	public Date getDateofcreation() {
+		return dateofcreation;
+	}
+
+	public void setDateofcreation(Date dateofcreation) {
+		this.dateofcreation = dateofcreation;
+	}
+
+	public Tour getTour() {
+		return tour;
+	}
+
+	public void setTour(Tour tour) {
+		this.tour = tour;
+	}
+	
+	
 
 }
